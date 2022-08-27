11d3b06a14b81c88bec75509c88164935ce4fccf
Ibrahima Sory TRAORE <ibrahima982011@gmail.com>
2022-08-27 17:37:33 +0200

    Fix issue: Using the delete button works but elements comes back when i refresh the page.

--- src/main/java/adeo/leroymerlin/cdp/EventRepository.java
+++ src/main/java/adeo/leroymerlin/cdp/EventRepository.java
@@ -1,13 +1,16 @@
 package adeo.leroymerlin.cdp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Transactional(readOnly = true)
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
+    @Transactional
     void deleteById(Long eventId);
 
     List<Event> findAllBy();
